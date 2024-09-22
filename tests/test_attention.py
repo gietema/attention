58b392edbab1ad987a377bebd943dc6737afbdc0
gietema <jochem@giete.ma>
2024-09-22 19:38:15 +0100

    Test with torch.nn.functional.scaled_dot_product_attention

--- tests/test_attention.py
+++ tests/test_attention.py
@@ -1,14 +1,15 @@
-from attention.flash_attention import flash_attention_forward
+from attention.attention import standard_attention
 import torch
 
-def test_flash_attention():
+
+def test_attention():
     torch.manual_seed(0)  # for reproducibility
     batch_size, seq_len, head_dim = 2, 1024, 64
     q = torch.randn(batch_size, seq_len, head_dim)
     k = torch.randn(batch_size, seq_len, head_dim)
     v = torch.randn(batch_size, seq_len, head_dim)
 
-    flash_output = flash_attention_forward(q, k, v)
+    flash_output = standard_attention(q, k, v)
     standard_output = torch.nn.functional.scaled_dot_product_attention(q, k, v)
 
     torch.testing.assert_close(standard_output, flash_output)
