2a2d107fb2c4fe89d2859e848bd7752b1f8f847e
gietema <jochem@giete.ma>
2024-09-22 19:47:53 +0100

    Reformat import

--- attention/flash_attention.py
+++ attention/flash_attention.py
@@ -4,8 +4,8 @@ FlashAttention: Fast and Memory-Efficient Exact Attention with IO-Awareness
 https://arxiv.org/abs/2205.14135
 """
 
-import torch
 import math
+import torch
 
 
 def compute_attention_block(
